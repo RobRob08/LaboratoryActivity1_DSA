6272731251b8e0ec72cc468abb6546caa1a069bd
qdraven <qdrvgarcia@tip.edu.ph>
2024-08-23 14:51:59 +0800

    Update LabProj.java
    void.get

--- LabProj/src/main/java/com/mycompany/labproj/LabProj.java
+++ LabProj/src/main/java/com/mycompany/labproj/LabProj.java
@@ -665,7 +665,8 @@ abstract class BookModification{
     }//GEN-LAST:event_add_bookMousePressed
 
     private void jLabel2MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MousePressed
-      
+        BookModification book = new BookMod();
+        book.get();
     }//GEN-LAST:event_jLabel2MousePressed
 
     private void remove_bookMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_remove_bookMousePressed
